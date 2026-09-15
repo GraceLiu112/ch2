9f700107eab5cc50e7d61184967a3ae1a9ed51f2
GraceLiu112 <liugrace943@gmail.com>
2026-09-15 12:45:00 -0700

    Add files via upload

--- Data.java
+++ Data.java
@@ -1,8 +1,21 @@
 public class Data {
 	public static void main(String[] args){
 		
-		System.out.println("Hello, World!");
-		String day = "Monday";		
+		//System.out.println("Hello, World!");
+		String day;
+		day = "Tuesday";	
+		int date;
+		date = 15;
+		String month;
+		month = "September";
+		int year;
+		year = 2026;
+		System.out.println(day);
+		System.out.println(date);
+		System.out.println(month);
+		System.out.println(year);
+		System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);	
+		System.out.println("European format: " + day + " " + date + " " + month + " " + year);				
 	}
 
 }
